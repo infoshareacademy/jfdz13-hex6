dad141649b20853b593c2a34d6812284fabb15ac
Adrianna Musialik <musialik.adrianna@gmail.com>
2019-12-14 09:50:13 +0100

    fixing js code - navbar is closing after clicking on mobile devices

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-
 const mobileViewport = window.matchMedia("screen and (max-width: 900px)");
 const desktopViewport = window.matchMedia("screen and (min-width: 901px)");
 
 
 const navigationContainer = document.getElementById("navigationContainer");
 const hamburgerIcon = document.getElementById('navigationTitle');
-const menuButtons = document.querySelectorAll('.navigation-list__item');
-console.log(menuButtons);
+const menuButtons = document.querySelectorAll('.navigation-link');
+
+
 
 hamburgerIcon.onclick = function () {
     if (mobileViewport.matches) {
@@ -14,17 +14,17 @@ hamburgerIcon.onclick = function () {
             navigationContainer.classList.add("navigation-container--closed");
         } else {
             navigationContainer.classList.remove("navigation-container--closed");
-     }
+        }
     }
 };
 
-menuButtons.onclick = function () {
-    if (mobileViewport.matches) {
-            navigationContainer.classList.add('navigation-container--closed');
-    }
-};
-
-
+menuButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        if (mobileViewport.matches) {
+            navigationContainer.classList.add("navigation-container--closed");
+        }
+    });
+});
 
 function hasScrolled() {
     if (desktopViewport.matches) {
@@ -35,14 +35,14 @@ function hasScrolled() {
             navigationContainer.classList.remove('navigation-container--invisible');
         }
     }
-}
+};
 
 document.addEventListener('scroll', function() {
   isScrolling = true;
 }, false);
 
 setInterval(function() {
-  if (isScrolling) {
+  if (isScrolling = true) {
     hasScrolled();
     isScrolling = false;
   }
